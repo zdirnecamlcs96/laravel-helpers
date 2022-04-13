1dfeac5a589f62cf9bba08a5a8d0c361d8c6dc67
Chia Sheng <chiasheng@etctech.com.my>
2022-04-13 11:27:18 +0800

    validation request issue

--- src/Traits/Helpers.php
+++ src/Traits/Helpers.php
@@ -2,11 +2,17 @@
 
 namespace Zdirnecamlcs96\Helpers\Traits;
 
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
 trait Helpers {
+
+    use ValidatesRequests;
 
     use Authentication, Env, FCM, FileSystem, Locale, Logging, SMS, Requests, Validation, Date, Str, Others, Sessions, Datatable {
         Logging::__normalLog insteadOf FCM, SMS;
         Env::__isDebug insteadOf Requests;
+        ValidatesRequests::getValidationFactory insteadOf Validation;
+        Validation::validate insteadOf ValidatesRequests;
     }
 
 }
